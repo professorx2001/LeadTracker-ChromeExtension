d11496f2bac3a8c7e8d5bc727f6e85b7b6f1bbe7
Md Zaki Hussain <mdzakihusain@gmail.com>
2023-06-20 22:36:17 +0530

    updated buttons

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let myLeads = []
+const delBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+//local storage stores string so I am using JSON.stringify to make it string and JSON.parse to make it whatever datatype it was.
+// localStorage.setItem("", ) and localStorage.getItem("") to store and access
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
-function renderLeads(){
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads){
     let listItems = ""
-    for(let i=0; i<myLeads.length; i++){
+    for(let i=0; i<leads.length; i++){
          listItems += `
          <li>
-             <a target='_blank' href="${myLeads[i]}"> 
-             ${myLeads[i]}
+             <a target='_blank' href="${leads[i]}"> 
+             ${leads[i]}
             </a>
          </li>`
     }
     ulEl.innerHTML = listItems
 }
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+delBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+    
+})
